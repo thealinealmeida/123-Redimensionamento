37ebbf106b6a1dcc8351ef49c9523b32e35d8e0a
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2024-01-16 19:04:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+var poseNet;
 
+// const canvas = {
+//     w: 550,
+//     h: 500
+// }
+const canvas = {}
+
+const sqr = {
+    s: 100,
+}
+
+function preload() {
+    canvas.top = document.querySelector("#header").clientHeight + 30;
+    canvas.w = windowWidth / 2;
+    canvas.h = windowHeight - canvas.top;
+    if (canvas.h > canvas.w) {
+        canvas.h = canvas.w;
+    }
+    sqr.x = canvas.w / 2;
+    sqr.y = canvas.h / 2;
+}
+
+function setup() {
+    var video = createCapture(VIDEO);
+    // video.size(canvas.w, canvas.w * 3 / 4);
+    video.size(canvas.w, canvas.h);
+    video.position(0, canvas.top);
+
+    var cnv = createCanvas(canvas.w, canvas.h);
+    cnv.position(canvas.w, canvas.top);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
