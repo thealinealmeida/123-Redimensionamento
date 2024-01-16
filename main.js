aba7e7755e4ab1fc08ea94f09874599c1952bf2a
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2024-01-16 19:34:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,30 @@ function setup() {
 
     poseNet = ml5.poseNet(video, modelLoaded);
 }
+//Hoje
+function draw() {
+    background('#05a4fa');
+    fill('#D7EBBA');
+    rectMode(CENTER);
+    square(sqr.x, sqr.y, sqr.s);
+    //plano de fundo, preenchimento, centralizar modo, posição do quadrado
+}
+
+function modelLoaded() {
+    console.log("poseNet is up!"); // carregar modelo
+    poseNet.on('pose', gotPoses); //ligar 
+}
+
+function gotPoses(results) {
+    //passar os modelo para o console
+
+    if(results.length > 0){
+        sqr.x = results[0].pose.nose.x;
+        sqr.y = results[0].pose.nose.y;
+
+
+    }
+
+
+
+}
